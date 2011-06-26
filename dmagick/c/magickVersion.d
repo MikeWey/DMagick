e12caad434ba02a862e65ab5a9a6cc68f8ea00c8
Mike Wey <mike@example.com>
2011-06-26 22:49:06 +0200

    Add the version of ImageMagick the headers are based on to dmagick.c.magickVersion

--- dmagick/c/magickVersion.d
+++ dmagick/c/magickVersion.d
@@ -2,6 +2,11 @@ module dmagick.c.magickVersion;
 
 extern(C)
 {
+	/// Defines the version of ImageMagick where these headers are based on.
+	enum MagickLibVersion = 0x666;
+	///ditto
+	enum MagickLibVersionText = "6.6.6"; 
+
 	char* GetMagickHomeURL();
 
 	const(char)* GetMagickCopyright();
